cb22a398d32c1fe2134bbd8d07009a4e5ba8cd21
Nicole Freed <nicole.freed@gmail.com>
2017-08-16 08:45:59 -0700

    move task model data and all methods except edit ones to task-list.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,10 +6,9 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
-    <ul>
-      <li (click)="toggleDone(currentTask)" [ngStyle]="setStyle(currentTask)" [class]="priorityColor(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask(currentTask)">Edit!</button>
-      </li>
-    </ul>
+    
+    <task-list></task-list>
+
     <hr>
         <div *ngIf="selectedTask">
           <h3>{{selectedTask.description}}</h3>
@@ -18,7 +17,6 @@ import { Component } from '@angular/core';
           <label>Enter Task Description:</label>
           <input [(ngModel)]="selectedTask.description">
           <br>
-
           <form id="edit">
             <label>Enter Task Priority (1-3):</label>
             <br>
@@ -39,11 +37,7 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course', 3),
-    new Task('Begin brainstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on GitHub', 2)
-  ];
+
   selectedTask: null;
 
   editTask(clickedTask) {
@@ -52,53 +46,6 @@ export class AppComponent {
 
   finishedEditing() {
     this.selectedTask = null;
-  }
-
-  toggleDone(currentTask) {
-    console.log('toggle');
-    // currentTask.done = !currentTask.done;
-    if (currentTask.done === false) {
-      currentTask.done = true;
-    } else {
-      currentTask.done = false;
-    }
-  }
-
-  setStyle(currentTask) {
-    // let style;
-    // if (currentTask.done) {
-    //   console.log('true');
-    //   style = {
-    //     'text-decoration': 'line-through'
-    //   }
-    // } else {
-    //   console.log('false');
-    //   style = {
-    //     'text-decoration': 'none'
-    //   }
-    // }
-    let style = {
-      'text-decoration': currentTask.done ? 'line-through': 'none'
-    };
-    return style;
-  }
-
-  // isDone(clickedTask: Task) {
-  //   if(clickedTask.done === true) {
-  //     alert("This task is done!");
-  //   } else {
-  //     alert("This task is not done. Better get to work!");
-  //   }
-  // }
-
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
   }
 }
 
