a1338b82618ec2678ebb690f6f5edb9b22514915
Nicole Freed <nicole.freed@gmail.com>
2017-08-16 09:59:43 -0700

    add edit-task element and component to root component

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,8 +7,9 @@ import { Task } from './task.model';
   <div class="container">
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
-
     <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
+    <hr>
+    <edit-task></edit-task>
   </div>
   `
 })
