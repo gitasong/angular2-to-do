dc2be6d8b58aacb699584f2408a89de954e7af27
Nicole Freed <nicole.freed@gmail.com>
2017-08-16 10:45:03 -0700

    create new-task.component.ts with imports, @Component, and export class

---
+++ app/new-task.component.ts
@@ -0,0 +1,13 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from './task.model';
+
+@Component({
+  selector: 'new-task',
+  template: `
+    <h1>New Task</h1>
+  `
+})
+
+export class NewTaskComponent {
+
+}
