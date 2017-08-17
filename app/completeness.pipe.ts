40e0bcae6def028ad9dfe41a032861780e57b73a
Nicole Freed <nicole.freed@gmail.com>
2017-08-17 08:24:10 -0700

    add transform method to pipe

--- app/completeness.pipe.ts
+++ app/completeness.pipe.ts
@@ -8,4 +8,14 @@ import {Task} from './task.model';
 
 export class CompletenessPipe implements PipeTransform {
 
+
+  transform(input: Task[], args) {
+    var output: Task[] = [];
+    for (var i = 0; i < input.length; i++) {
+      if (input[i].done === false) {
+        output.push(input[i]);
+      }
+    }
+    return output;
+  }
 }
